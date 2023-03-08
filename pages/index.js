20d5612cdea61f9eb4fd08c000f9a6fa7d2d9bf5
Lee Kol <lee.levy333@gmail.com>
2023-03-08 17:48:07 +0200

    The contact section was added

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,18 @@ import Hero from "../components/home-page/hero";
 import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
+import Head from "next/head";
 
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Lee's Blog</title>
+        <meta
+          name="description"
+          content="I post about progremmimg and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,18 @@
 import { getPostData, getPostsFiles } from "../../lib/posts-util";
 import PostContent from "../../components/posts/post-detail/post-content";
+import { Fragment } from "react";
+import Head from "next/head";
 
 function PostDetailsPage(props) {
-  return <PostContent post={props.post} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>{props.post.title}</title>
+        <meta name="description" content={props.post.excerpt}/>
+      </Head>
+      <PostContent post={props.post} />;
+    </Fragment>
+  );
 }
 
 export function getStaticProps(context) {
